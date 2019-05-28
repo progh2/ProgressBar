4a9b77a1af516a03bdaf63e6e90343636cff3828
Gihun Ham <progh2@gmail.com>
2019-05-28 10:15:44 +0900

    #2 ProgressBar 코드 구현

--- ProgressBar/Form1.Designer.cs
+++ ProgressBar/Form1.Designer.cs
@@ -29,41 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pbStatus1 = new System.Windows.Forms.ProgressBar();
-            this.pbStatus2 = new System.Windows.Forms.ProgressBar();
-            this.pbStatus3 = new System.Windows.Forms.ProgressBar();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
             this.lblStatus = new System.Windows.Forms.Label();
             this.btnRun = new System.Windows.Forms.Button();
             this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // pbStatus1
+            // pbStatus
             // 
-            this.pbStatus1.Location = new System.Drawing.Point(12, 12);
-            this.pbStatus1.Name = "pbStatus1";
-            this.pbStatus1.Size = new System.Drawing.Size(273, 35);
-            this.pbStatus1.TabIndex = 0;
-            // 
-            // pbStatus2
-            // 
-            this.pbStatus2.Location = new System.Drawing.Point(12, 53);
-            this.pbStatus2.Name = "pbStatus2";
-            this.pbStatus2.Size = new System.Drawing.Size(273, 44);
-            this.pbStatus2.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.pbStatus2.TabIndex = 1;
-            // 
-            // pbStatus3
-            // 
-            this.pbStatus3.Location = new System.Drawing.Point(12, 103);
-            this.pbStatus3.Name = "pbStatus3";
-            this.pbStatus3.Size = new System.Drawing.Size(273, 42);
-            this.pbStatus3.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-            this.pbStatus3.TabIndex = 2;
+            this.pbStatus.Location = new System.Drawing.Point(12, 12);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(368, 35);
+            this.pbStatus.TabIndex = 0;
             // 
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(12, 284);
+            this.lblStatus.Location = new System.Drawing.Point(12, 145);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(52, 15);
             this.lblStatus.TabIndex = 3;
@@ -71,31 +55,56 @@
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(12, 151);
+            this.btnRun.BackColor = System.Drawing.Color.Red;
+            this.btnRun.Font = new System.Drawing.Font("굴림", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRun.ForeColor = System.Drawing.Color.White;
+            this.btnRun.Location = new System.Drawing.Point(12, 53);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(273, 130);
+            this.btnRun.Size = new System.Drawing.Size(186, 89);
             this.btnRun.TabIndex = 4;
             this.btnRun.Text = "실행";
-            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.BtnRun_Click);
             // 
             // Timer
             // 
-            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(207, 104);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(173, 38);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "리셋";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(207, 60);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(173, 38);
+            this.btnEnd.TabIndex = 6;
+            this.btnEnd.Text = "멈춤";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.BtnEnd_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 315);
+            this.ClientSize = new System.Drawing.Size(394, 179);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.lblStatus);
-            this.Controls.Add(this.pbStatus3);
-            this.Controls.Add(this.pbStatus2);
-            this.Controls.Add(this.pbStatus1);
+            this.Controls.Add(this.pbStatus);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "상태진행";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,12 +112,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar pbStatus1;
-        private System.Windows.Forms.ProgressBar pbStatus2;
-        private System.Windows.Forms.ProgressBar pbStatus3;
+        private System.Windows.Forms.ProgressBar pbStatus;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEnd;
     }
 }
 
